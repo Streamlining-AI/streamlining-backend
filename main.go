ac819b9a23eb7f92933670158d7eb667418d0d88
Nitipat Boongate <62010494@kmitl.ac.th>
2022-12-07 16:44:02 +0700

    [Update] Add Cors

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	controller "github.com/Streamlining-AI/streamlining-backend/controllers"
 	// middleware "github.com/Streamlining-AI/streamlining-backend/middleware"
 	routes "github.com/Streamlining-AI/streamlining-backend/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 	"github.com/joho/godotenv"
@@ -21,6 +22,15 @@ func init() {
 func main() {
 	router := gin.New()
 	router.Use(gin.Logger())
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "OPTION"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
 	routes.UserRoutes(router)
 
 	router.GET("/upload/clone", controller.HandlerUpload())
